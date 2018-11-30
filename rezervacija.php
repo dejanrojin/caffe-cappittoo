b5d3483613ed39a665d2bce14c170b8e6e47542a
dejanrojin <dejan.rojin@yahoo.com>
2018-11-30 23:41:47 +0100

    Add files via upload

---
+++ rezervacija.php
@@ -0,0 +1,29 @@
+
+<div id="rezervacija">
+
+
+
+<div class="col-sm-4">
+<div class="row">
+
+</div></div>
+
+
+<div class="col-sm-4">
+<div class="row">
+<div id="broj"> Reservacije Kappitoo<br> +381 61612088</div>
+
+</div></div>
+
+
+
+<div class="col-sm-4">
+<div class="row">
+
+
+</div>
+</div>
+
+
+
+<!-- Tend of rezervacija --> </div>
